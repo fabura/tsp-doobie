972dab2e2dd5e48276078fbb48ed87ed44b825df
Kuzma Leshakov <leshakov.kuzma@clover.global>
2019-07-22 07:37:46 +0300

    Separate tests

--- src/test/scala/InsertManySpec.scala
+++ src/test/scala/InsertManySpec.scala
@@ -10,37 +10,15 @@ class InsertManySpec extends Specification {
  This is my first test
 
  This SQL query string should
-   be equal 'User(1,Anton)User(2,Sergei)User(3,Ivan)User(4,John)'  $e1
    This queue should contain 1kk elements                          $e2
                                                                  """
 
   val runtime: DefaultRuntime = new DefaultRuntime {}
 
-  def e1 = "User(1,Anton)User(2,Sergei)User(3,Ivan)User(4,John)" must_== runtime.unsafeRun(program)
-
   val tableValues: List[List[String]] = List(List("id", "int"), List("name", "varchar"))
   val tableName: String = "Users"
   val tableColumns: List[String] = List("id", "name")
   val millionUsers: List[User] = (1 to 500).toList.map(User(_, "Vasya"))
-
-
-  val program: ZIO[Any, Throwable, String] = for {
-    container   <- ZIO(PostgreSQLContainer())
-    _           <- IO.effectTotal(container.start())
-    xa          =  getTransactor(container)
-    dbInterface =  DatabaseInterface(xa)
-    _           <- dbInterface.createTable(tableName, tableValues)
-    _           <- dbInterface.insertOne(User(1,"Anton"))
-    _           <- dbInterface.insertOne(User(2,"Sergei"))
-    _           <- dbInterface.insertOne(User(3,"Ivan"))
-    _           <- dbInterface.insertOne(User(4,"John"))
-
-    queue       <- dbInterface.getQueue(100)
-    queueData   <- queue.takeAll
-
-  } yield queueData.mkString
-
-
 
 
   def e2 = runtime.unsafeRun(
